c1935c0eb75a5007d852a6dbe88205fca1c10430
valentin <valentin.lalue1998@gmail.com>
2024-06-06 18:17:29 +0200

    "test 1061"

--- login.php
+++ login.php
@@ -1,21 +1,24 @@
 <?php
 session_start();
 $bdd = new PDO('mysql:host=localhost;dbname=sae23;charset=utf8;', 'rt', 'enzolebg');
-if(isset($_POST['envoi'])){ //si utilisateur appuie sur le bouton d'envoi
-    if(!empty($_POST['pseudo']) AND !empty($_POST['mdp'])){ //si les champs ne sont pas vides
-        $pseudo = htmlspecialchars($_POST['pseudo']); //défini la variable pseudo en se protégant des injections grâce au htmlspecialchars
-        $mdp = password_hash($_POST['mdp']); //défini la variable mdp en la chiffrant avec du sha1
-        $recupUser = $bdd->prepare('SELECT * FROM administration WHERE pseudo = ? AND mdp = ?'); //récupère l'utilisateur dans la table administration auquel correspond les champs donnés
-        $recupUser->execute(array($pseudo, $mdp)); //renvoie un tableau avec nos champs
+if (isset($_POST['envoi'])) { // si l'utilisateur appuie sur le bouton d'envoi
+    if (!empty($_POST['pseudo']) && !empty($_POST['mdp'])) { // si les champs ne sont pas vides
+        $pseudo = htmlspecialchars($_POST['pseudo']); // protection contre les injections
+        $mdp = $_POST['mdp']; // récupération du mot de passe non haché
 
-        if($recupUser->rowCount() > 0){ // si on a au moins un des deux champs de notre tableau qui est rempli, ce qui prouve que l'utilisateur est dans notre table, alors on le connecte
+        // Préparation de la requête pour récupérer l'utilisateur
+        $recupUser = $bdd->prepare('SELECT * FROM administration WHERE pseudo = ?');
+        $recupUser->execute(array($pseudo));
+        $user = $recupUser->fetch();
+
+        if ($user && password_verify($mdp, $user['mdp'])) { // vérification du mot de passe
             $_SESSION['pseudo'] = $pseudo;
-            $_SESSION['mdp'] = $mdp;
             header('Location: index.php');
-        }else{
+            exit();
+        } else {
             echo "Vos informations sont incorrectes";
         }
-    }else{//si les champs sont vides on demande a l'utilisateur de les remplir
+    } else { // si les champs sont vides
         echo "Veuillez compléter tous les champs";
     }
 }
