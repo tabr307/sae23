afae4558b3ddfea8aaad92be34531ec7b686bdc3
valentin <valentin.lalue1998@gmail.com>
2024-06-06 11:32:59 +0200

    "test 1004"

--- admin/administration.php
+++ admin/administration.php
@@ -1,14 +1,14 @@
 <!DOCTYPE html>
 
 <?php
-if (!isset($_SERVER['rt'])) {
+if (!isset($_SERVER['PHP_AUTH_USER'])) {
     header('WWW-Authenticate: Basic realm="My Realm"');
     header('HTTP/1.0 401 Unauthorized');
     echo 'Texte utilisé si le visiteur utilise le bouton d\'annulation';
     exit;
 } else {
-    echo "<p>Bonjour, {$_SERVER['rt']}.</p>";
-    echo "<p>Votre mot de passe est {$_SERVER['enzolebg']}.</p>";
+    echo "<p>Bonjour, {$_SERVER['PHP_AUTH_USER']}.</p>";
+    echo "<p>Votre mot de passe est {$_SERVER['PHP_AUTH_PW']}.</p>";
 }
 ?>
 
