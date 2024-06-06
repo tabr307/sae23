c9762593202b09e375cc68a948117afd24f2e6e6
valentin <valentin.lalue1998@gmail.com>
2024-06-06 17:47:02 +0200

    "test 1051"

--- login.php
+++ login.php
@@ -5,7 +5,7 @@ if(isset($_POST['envoi'])){ //si utilisateur appuie sur le bouton d'envoi
     if(!empty($_POST['pseudo']) AND !empty($_POST['mdp'])){ //si les champs ne sont pas vides
         $pseudo = htmlspecialchars($_POST['pseudo']); //défini la variable pseudo en se protégant des injections grâce au htmlspecialchars
         $mdp = sha1($_POST['mdp']); //défini la variable mdp en la chiffrant avec du sha1
-        $recupUser = $bdd->prepare('SELECT * FROM aministration WHERE login = ? AND mdp = ?'); //récupère l'utilisateur dans la table administration auquel correspond les champs donnés
+        $recupUser = $bdd->prepare('SELECT * FROM `aministration` WHERE `pseudo` = ? AND `mdp` = ?'); //récupère l'utilisateur dans la table administration auquel correspond les champs donnés
         $recupUser->execute(array($pseudo, $mdp)); //renvoie un tableau avec nos champs
 
         if($recupUser->rowCount() > 0){ // si on a au moins un des deux champs de notre tableau qui est rempli, ce qui prouve que l'utilisateur est dans notre table, alors on le connecte
