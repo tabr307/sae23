1dbe80ea800e9dc4c29cedf51a6633c02c6c1a47
valentin <valentin.lalue1998@gmail.com>
2024-06-06 10:28:15 +0200

    "script fix"

--- admin/administration.php
+++ admin/administration.php
@@ -1,165 +1,56 @@
+<!DOCTYPE html>
+
 <?php
-session_start();
-
-// Configuration de la base de données
-$host = '83.113.15.31'; // Adresse du serveur de la base de données (généralement localhost)
-$dbname = 'sae23'; // Nom de la base de données
-$username = 'rt'; // Nom d'utilisateur de la base de données
-$password = 'enzolebg'; // Mot de passe de la base de données
-
-// Connexion à la base de données
-try {
-    $pdo = new PDO("mysql:host=$host;dbname=$dbname", $username, $password);
-    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-} catch (PDOException $e) {
-    die("Erreur de connexion : " . $e->getMessage());
+if (!isset($_SERVER['rt'])) {
+    header('WWW-Authenticate: Basic realm="My Realm"');
+    header('HTTP/1.0 401 Unauthorized');
+    echo 'Texte utilisé si le visiteur utilise le bouton d\'annulation';
+    exit;
+} else {
+    echo "<p>Bonjour, {$_SERVER['rt']}.</p>";
+    echo "<p>Votre mot de passe est {$_SERVER['enzolebg']}.</p>";
 }
-
-// Vérification du formulaire de connexion
-if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-    $login = $_POST['login'];
-    $mdp = $_POST['mdp'];
-
-    // Vérification des informations d'identification
-    if ($login === 'rt' && $mdp === 'enzolebg') {
-        $_SESSION['loggedin'] = true;
-        header('Location: administration.php');
-        exit;
-    } else {
-        $error = 'Login ou mot de passe incorrect.';
-    }
-}
-
-// Vérification de la session
-if (!isset($_SESSION['loggedin']) || $_SESSION['loggedin'] !== true) {
 ?>
-    <!DOCTYPE html>
-    <html>
+
+<html>
     <head>
-        <title>Connexion</title>
+        <meta charset="UTF-8">
+        <title>Administration</title>
+        <link href="./../styles/main.css" rel="stylesheet">
+        <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200" />
     </head>
     <body>
-        <h2>Connexion</h2>
-        <?php if (isset($error)) { echo '<p style="color:red;">' . $error . '</p>'; } ?>
-        <form method="post" action="administration.php">
-            <label for="login">Login :</label>
-            <input type="text" name="login" id="login" required><br>
-            <label for="mdp">Mot de passe :</label>
-            <input type="password" name="mdp" id="mdp" required><br>
-            <button type="submit">Se connecter</button>
-        </form>
+        <section class="navbar">
+            <a class="active" href="./../index.php"><img src="./../ressources/logo.png" id="image1"alt="logo"></a> 
+            <section class="links">
+            <a class="right" href="#"> Gestion de Projet</a> 
+            <a class="right" href="./../consult/consultation.php"> Consultation</a> 
+            <a class="right" href="./../gestion/gestion.php"> Gestion</a> 
+            <a class="right" href="administration.php"> Administration</a>
+            </section>
+        </section>
+        <section class="container">
+            <section class="content">
+            </section>
+        </section>
+        <aside id="last">
+            <hr />
+            <p><em> Validation de la page HTML5 - CSS3 </em></p>
+            <a href="https://validator.w3.org/nu/?doc=http%3A%2F%2Flalue.atwebpages.com%2FTP_R109%2Frwd.html" target="_blank"> 
+                <img class= "image-responsive" src="images/html5-validator-badge-blue.png" alt="HTML5 Valide !" />
+            </a>
+            &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; 
+            <a href="http://jigsaw.w3.org/css-validator/validator?uri=http%3A%2F%2Flalue.atwebpages.com%2FTP_R109%2Fstyles%2Fstyle2RWD.css" target="_blank">
+                <img class= "image-responsive" src="http://jigsaw.w3.org/css-validator/images/vcss-blue" alt="CSS Valide !" />
+            </a>
+          </aside>
+            
+          <footer>
+            <ul>
+              <li>Département Réseaux et Télécommunications</li>
+              <li>Cazettes, Le Deunff, Muller, Lalue</li>
+              <li>BUT1&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</li>
+            </ul>  
+          </footer>
     </body>
-    </html>
-<?php
-    exit;
-}
-
-// Affichage des données si l'utilisateur est connecté
-$queryBatiments = $pdo->query('SELECT * FROM batiment');
-$querySalles = $pdo->query('SELECT * FROM salle');
-$queryCapteurs = $pdo->query('SELECT * FROM capteur');
-$queryMesures = $pdo->query('SELECT * FROM mesures');
-$queryAdmin = $pdo->query('SELECT * FROM administration');
-?>
-
-<!DOCTYPE html>
-<html>
-<head>
-    <title>Administration</title>
-</head>
-<body>
-    <h2>Administration - Données des Tables</h2>
-
-    <h3>Batiments</h3>
-    <table border="1">
-        <tr>
-            <th>ID</th>
-            <th>Nom</th>
-            <th>Login</th>
-            <th>Mot de Passe</th>
-        </tr>
-        <?php while ($row = $queryBatiments->fetch(PDO::FETCH_ASSOC)) { ?>
-            <tr>
-                <td><?php echo htmlspecialchars($row['id_batiment']); ?></td>
-                <td><?php echo htmlspecialchars($row['nom_batiment']); ?></td>
-                <td><?php echo htmlspecialchars($row['login']); ?></td>
-                <td><?php echo htmlspecialchars($row['mdp']); ?></td>
-            </tr>
-        <?php } ?>
-    </table>
-
-    <h3>Salles</h3>
-    <table border="1">
-        <tr>
-            <th>ID</th>
-            <th>Nom</th>
-            <th>Type</th>
-            <th>Capacité</th>
-            <th>ID Batiment</th>
-        </tr>
-        <?php while ($row = $querySalles->fetch(PDO::FETCH_ASSOC)) { ?>
-            <tr>
-                <td><?php echo htmlspecialchars($row['id_salle']); ?></td>
-                <td><?php echo htmlspecialchars($row['nom_salle']); ?></td>
-                <td><?php echo htmlspecialchars($row['type']); ?></td>
-                <td><?php echo htmlspecialchars($row['capacite']); ?></td>
-                <td><?php echo htmlspecialchars($row['id_batiment']); ?></td>
-            </tr>
-        <?php } ?>
-    </table>
-
-    <h3>Capteurs</h3>
-    <table border="1">
-        <tr>
-            <th>ID</th>
-            <th>Nom</th>
-            <th>Type</th>
-            <th>Unité</th>
-            <th>ID Salle</th>
-        </tr>
-        <?php while ($row = $queryCapteurs->fetch(PDO::FETCH_ASSOC)) { ?>
-            <tr>
-                <td><?php echo htmlspecialchars($row['id_capteur']); ?></td>
-                <td><?php echo htmlspecialchars($row['nom_capteur']); ?></td>
-                <td><?php echo htmlspecialchars($row['type']); ?></td>
-                <td><?php echo htmlspecialchars($row['unite']); ?></td>
-                <td><?php echo htmlspecialchars($row['id_salle']); ?></td>
-            </tr>
-        <?php } ?>
-    </table>
-
-    <h3>Mesures</h3>
-    <table border="1">
-        <tr>
-            <th>ID</th>
-            <th>Date</th>
-            <th>Horaire</th>
-            <th>Valeur</th>
-            <th>ID Capteur</th>
-        </tr>
-        <?php while ($row = $queryMesures->fetch(PDO::FETCH_ASSOC)) { ?>
-            <tr>
-                <td><?php echo htmlspecialchars($row['id_mesure']); ?></td>
-                <td><?php echo htmlspecialchars($row['date']); ?></td>
-                <td><?php echo htmlspecialchars($row['horaire']); ?></td>
-                <td><?php echo htmlspecialchars($row['valeur']); ?></td>
-                <td><?php echo htmlspecialchars($row['id_capteur']); ?></td>
-            </tr>
-        <?php } ?>
-    </table>
-
-    <h3>Administration</h3>
-    <table border="1">
-        <tr>
-            <th>Login</th>
-            <th>Mot de Passe</th>
-        </tr>
-        <?php while ($row = $queryAdmin->fetch(PDO::FETCH_ASSOC)) { ?>
-            <tr>
-                <td><?php echo htmlspecialchars($row['login']); ?></td>
-                <td><?php echo htmlspecialchars($row['mdp']); ?></td>
-            </tr>
-        <?php } ?>
-    </table>
-</body>
 </html>
