4401e2a996ac6fe0453a2f0c7dc2783ea2ffef5a
valentin <valentin.lalue1998@gmail.com>
2024-06-07 15:19:59 +0200

    "test smi3"

--- config.php
+++ config.php
@@ -1,7 +1,7 @@
 <?php
 /* Script de connexion à la base smi */
 
-  $id_bd = new PDO('mysql:host=localhost;dbname=sae23;charset=utf8;','rt','enzolebg');
+  $id_bd = new PDO('mysql:host=localhost;dbname=sae23;charset=utf8;','rt','enzolebg')
     or die("Connexion au serveur et/ou à la base de données impossible");
 
   /* Gestion de l'encodage des caractères */
