eae8a7e0689534be7066bb3dc352005032fa1a22
valentin <valentin.lalue1998@gmail.com>
2024-06-07 15:46:49 +0200

    "test smi8"

--- test/login.php
+++ test/login.php
@@ -1,36 +1,81 @@
-<?php
-	session_start();
-	$_SESSION["mdp"]=$_REQUEST["mdp"];  // Récupération du mot de passe
-	$motdep=$_SESSION["mdp"];
-	$_SESSION["auth"]=FALSE;
+/<?php
+	//session_start();
+	//$_SESSION["mdp"]=$_REQUEST["mdp"];  // Récupération du mot de passe
+	//$motdep=$_SESSION["mdp"];
+	//$_SESSION["auth"]=FALSE;
 
 	// Script de vérification du mot de passe d'administration, en utilisant la table Connexion
 
-	if(empty($motdep))
-		header("Location:login_error.php");
-	else
-     {
+	//if(empty($motdep))
+	//header("Location:login_error.php");
+	//else
+    // {
 		/* Accès à la base */
-		include ("mysql.php");
+		//include ("mysql.php");
 
-		$requete = "SELECT `mdp` FROM `administration`;";
-		$resultat = mysqli_query($id_bd, $requete)
-			or die("Execution de la requete impossible : $requete");
+		//$requete = "SELECT `mdp` FROM `administration`;";
+		//$resultat = mysqli_query($id_bd, $requete)
+		//or die("Execution de la requete impossible : $requete");
 
-		$ligne = mysqli_fetch_row($resultat);
-		if ($motdep==$ligne[0])
-		 {
-			$_SESSION["auth"]=TRUE;		
+		//$ligne = mysqli_fetch_row($resultat);
+		//if ($motdep==$ligne[0])
+		// {
+			//$_SESSION["auth"]=TRUE;		
+			// mysqli_close($id_bd);
+			//echo "<script type='text/javascript'>document.location.replace('choix_type.php');</script>";
+			// }
+			//else
+			// {
+				//	$_SESSION = array(); // Réinitialisation du tableau de session
+				//  session_destroy();   // Destruction de la session
+				//unset($_SESSION);    // Destruction du tableau de session
+				// mysqli_close($id_bd);
+				// echo "<script type='text/javascript'>document.location.replace('login_error.php');</script>";
+				//}
+				//  } 
+
+session_start();
+$_SESSION["mdp"] = $_REQUEST["mdp"];  // Récupération du mot de passe
+$motdep = $_SESSION["mdp"];
+$_SESSION["auth"] = FALSE;
+
+// Script de vérification du mot de passe d'administration, en utilisant la table Connexion
+
+if (empty($motdep)) {
+    header("Location: login_error.php");
+    exit(); // Assurez-vous de terminer le script après la redirection
+} else {
+    /* Accès à la base */
+    include("mysql.php");
+
+    // Utilisation de requête préparée pour éviter les injections SQL
+    $requete = "SELECT `mdp` FROM `administration` WHERE `user` = ?"; // Assurez-vous que la colonne `user` existe dans votre table
+    if ($stmt = mysqli_prepare($id_bd, $requete)) {
+        mysqli_stmt_bind_param($stmt, "s", $username); // Remplacez `$username` par la variable contenant le nom d'utilisateur si nécessaire
+        mysqli_stmt_execute($stmt);
+        mysqli_stmt_bind_result($stmt, $hashed_password);
+        mysqli_stmt_fetch($stmt);
+
+        // Vérification du mot de passe avec `password_verify`
+        if (password_verify($motdep, $hashed_password)) {
+            $_SESSION["auth"] = TRUE;
+            mysqli_stmt_close($stmt);
             mysqli_close($id_bd);
-			echo "<script type='text/javascript'>document.location.replace('choix_type.php');</script>";
-		 }
-		else
-		 {
-			$_SESSION = array(); // Réinitialisation du tableau de session
+            echo "<script type='text/javascript'>document.location.replace('choix_type.php');</script>";
+            exit(); // Assurez-vous de terminer le script après la redirection
+        } else {
+            // Réinitialisation et destruction de la session en cas d'échec
+            $_SESSION = array(); // Réinitialisation du tableau de session
             session_destroy();   // Destruction de la session
             unset($_SESSION);    // Destruction du tableau de session
+            mysqli_stmt_close($stmt);
             mysqli_close($id_bd);
             echo "<script type='text/javascript'>document.location.replace('login_error.php');</script>";
-		 }
-     } 
- ?>
+            exit(); // Assurez-vous de terminer le script après la redirection
+        }
+    } else {
+        die("Échec de la préparation de la requête : " . mysqli_error($id_bd));
+    }
+}
+?>
+
