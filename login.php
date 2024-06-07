039f635591bf3c4ca3834cd797c76044f01e7686
valentin <valentin.lalue1998@gmail.com>
2024-06-07 16:55:27 +0200

    "test idk"

--- login.php
+++ login.php
@@ -7,7 +7,7 @@
 	// Script de vérification du mot de passe d'administration, en utilisant la table Connexion
 
 	if(empty($motdep))
-		header("Location:login_error.php");
+		header("Location:index.php");
 	else
      {
 		/* Accès à la base */
