610a0c1c78201a2c7e0594804fabba3703bf7e44
valentin <valentin.lalue1998@gmail.com>
2024-06-07 15:37:16 +0200

    "test smi5"

--- test/mysql.php
+++ test/mysql.php
@@ -1,7 +1,7 @@
 <?php
 /* Script de connexion à la base smi */
 
-  $id_bd = mysqli_connect("fdb31.eohost.com", "4379915_smi", "BUT1_RT_da-silva", "4379915_smi")
+  $id_bd = mysqli_connect("localhost", "rt", "enzolebg", "sae23")
     or die("Connexion au serveur et/ou à la base de données impossible");
 
   /* Gestion de l'encodage des caractères */
