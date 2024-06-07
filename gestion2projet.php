ac99eb500adccda0dbe60b81df85a108a0bf9a1a
valentin <valentin.lalue1998@gmail.com>
2024-06-07 17:49:11 +0200

    "remise en style de plein de choss"

--- gestion2projet.php
+++ gestion2projet.php
@@ -18,26 +18,31 @@
         </section>
         <section class="container">
             <section class="content">
-                <h1> Le GANTT final</h1>
+                <h1 id="titre32"> Le GANTT final</h1>
+                
                 <img class=".image"  src="ressources/gantt.PNG" alt="gantt">
-                <p>Ici, nous observons le Gantt prévisionnel réalisé pour le livrable 1. <br>
-                    En effet, nous avons essayé de planifier au mieux notre projet afin de ne pas perdre
-                    du temps ou faire des erreurs inutiles. <br>
-                    Or, bien que nos prévisions se sont avérées plutôt exactes, nous avons avons été
-                    obligés de faire quelques changements afin d'optimiser notre avancée et palier aux 
-                    problèmes rencontrés.
-                    
-                </p>
-                <h1>Screen GitHub</h1>
+                
                 <p>
-                    Je vous laisse chosir les bon screen à mettre
-                    <img class=".image" src="ressources/github.PNG" alt="gihb">
+                Ici, nous observons le Gantt prévisionnel réalisé pour le livrable 1. <br>
+                En effet, nous avons essayé de planifier au mieux notre projet afin de ne pas perdre
+                du temps ou faire des erreurs inutiles. <br>
+                Or, bien que nos prévisions se sont avérées plutôt exactes, nous avons avons été
+                obligés de faire quelques changements afin d'optimiser notre avancée et palier aux 
+                problèmes rencontrés.
                 </p>
+
+                <h1>Screen GitHub</h1>
+                
+                <p>
+                Je vous laisse chosir les bon screen à mettre
+                <img class=".image" src="ressources/github.PNG" alt="gihb">
+                </p>
+
                 <h1>Synthèses Personelles du groupe</h1>
-                <ul>
+                
                 <p>
                 <h2>Synthèse Personelle d'Hélio LE DEUNFF</h2> 
-                <img class=".image" src="ressources/helio.png" alt="helio"><br>
+                <img class=".image" src="ressources/helio.png" alt="helio">
                 Pour ma part j'ai commencé par la création du gantt prévisionnel lors de la première séance. En effet, après avoir 
                 discuté avec tous les membres du groupe et la révision du cahier des charges, j'ai pu mettre
                 au point un Gantt correcte est approuvé par l'équipe. <br>
@@ -48,29 +53,26 @@
                 Pour ce qui est de la 4ème séance, je me suis principalement occupé de la page "Gestion de projet" du site web,
                 j'ai avancé la partie informative de la page tandis que Valentin se chargeait de l'habillage et de la mise en page. <br>
                 Pour ce qui est de la 5 ème séance ... <br>
-                <h3>Conclusion :  </h3><br>
+                <h3>Conclusion :  </h3>
                 Pour conclure, j'ai essayé d'apporter mon aide de la manière la plus optimisé possible,
                 même si je n'ai pas été le plus actif sur la partie github et PHP.
-
-
                 </p>
 
-                <p>
+                
+
+                
                     <h2>Synthèse Personelle de Mathys CAZETTES</h2>
                     <img class=".image" src="ressources/8155751-1400x933.jpg" alt="mathys">
-                </p>
+                
 
-                <p>
+                
                     <h2>Synthèse Personelle de Valentin LALUE</h2>
                     <img class=".image" src="ressources/M6_0054095_0380bis-1140x856.jpg" alt="valentin">
-                </p>
 
-                <p>
                     <h2>Synthèse Personelle de Enzo MULLER</h2>
                     <img class=".image" src=".ressources/images.jfif" alt="enzo">
-                </p>
 
-            </ul>
+                
                 <h1>Problèmes rencontrés et Solutions proposées</h1>
 
                 <table>
@@ -124,9 +126,11 @@
                     sommes arrivées à la conclusion que notre avis sur le cahier des charges était plutôt positif.
                     En effet, (arguments)
                 </p>
+                <br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br>
+          
             </section>
         </section>
-                  
+                  <br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br>
           <footer>
             <ul>
               <li>Département Réseaux et Télécommunications</li>
