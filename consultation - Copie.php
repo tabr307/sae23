f4fbb5b85d774095f86c901329111eabe1b320f1
smashpasspass <helio.vilanove.iut@gmail.com>
2024-06-12 15:58:07 +0200

    Add files via upload
    test

---
+++ consultation - Copie.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Consultation</title>
+</head>
+<body>
+    <?php
+        $prenom = "H2LIO" <br>;
+        echo "bonjour $prenom";
+    
+    
+    
+   
+   
+   
+   
+   
+     ?>
+</body>   
+</html>
