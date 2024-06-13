0907bfd25eaa24c16df7b38ed8d33eccd6c9c288
helio <helio.vilanove.iut@gmail.com>
2024-06-13 20:58:43 +0200

    test

--- batBaction.php
+++ batBaction.php
@@ -58,14 +58,7 @@ tbody tr:hover {
     background-color: #f1f1f1;
 } </style>';
     
-    
-    if (in_array($salle, $validSalles) && in_array($capteur, $validCapteurs) && in_array($plage, $validPlages)) {
-        header('Location: Infogest.php');
-        exit();
-    } else {
-        // Redirection vers le formulaire avec un message d'erreur
-        echo "ya pb avec le array"
-    }
+   
 } else {
     // Redirection vers le formulaire si l'accès n'est pas via POST
     header("Location: gestion.php");
