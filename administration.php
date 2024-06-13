e8438aa3f02f96e7f1adbab725ae2e5e34af095f
valentin <valentin.lalue1998@gmail.com>
2024-06-13 13:15:51 +0200

    "ajout premier form page admin"

--- administration.php
+++ administration.php
@@ -13,36 +13,34 @@ session_start();
     <section class="navbar">
             <a class="active" href="index.php"><img src="ressources/logo.png" id="image1"alt="logo"></a> 
             <section class="links">
-            <a class="right" href="gestion2projet.php"> Gestion de Projet</a> 
-            <a class="right" href="consultation.php"> Consultation</a> 
-            <a class="right" href="form.php"> Gestion</a> 
-            <a class="right" href="form.php"> Administration</a>
+                <a class="right" href="gestion2projet.php"> Gestion de Projet</a> 
+                <a class="right" href="consultation.php"> Consultation</a> 
+                <a class="right" href="form.php"> Gestion</a> 
+                <a class="right" href="form.php"> Administration</a>
             </section>
         </section>
         <section class="container">
             <section class="content">
                 <?php echo "Bienvenue";
                 echo $_SESSION['pseudo'];?>
+                 <form method="POST" action="login.php" align="center">
+                    <input type="radio" name="table" value="table" id="bt_table">
+                    <label for="reseau">une table</label>
+                    <input type="radio" name="bd" value="bd" id="bt_bd">
+                    <label for="reseau">une base de données</label>
+                    <input type="radio" name="val" value="val" id="bt_val">
+                    <label for="reseau">une valeur</label>
+                    <input type="submit" value="Soumettre">
+                </form>
+        
             </section>
-        </section>
-        <aside id="last">
-            <hr />
-            <p><em> Validation de la page HTML5 - CSS3 </em></p>
-            <a href="https://validator.w3.org/nu/?doc=http%3A%2F%2Flalue.atwebpages.com%2FTP_R109%2Frwd.html" target="_blank"> 
-                <img class= "image-responsive" src="images/html5-validator-badge-blue.png" alt="HTML5 Valide !" />
-            </a>
-            &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; 
-            <a href="http://jigsaw.w3.org/css-validator/validator?uri=http%3A%2F%2Flalue.atwebpages.com%2FTP_R109%2Fstyles%2Fstyle2RWD.css" target="_blank">
-                <img class= "image-responsive" src="http://jigsaw.w3.org/css-validator/images/vcss-blue" alt="CSS Valide !" />
-            </a>
-          </aside>
-            
-          <footer>
-            <ul>
-              <li>Département Réseaux et Télécommunications</li>
-              <li>Cazettes, Le Deunff, Muller, Lalue</li>
-              <li>BUT1&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</li>
-            </ul>  
+        </section>            
+            <footer>
+                <ul>
+                    <li>Département Réseaux et Télécommunications</li>
+                    <li>Cazettes, Le Deunff, Muller, Lalue</li>
+                    <li>BUT1&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</li>
+                </ul>  
           </footer>
     </body>
 </html>
