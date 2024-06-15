be74af1022cc6d7303f6e8121f12c668c859bf88
valentin <valentin.lalue1998@gmail.com>
2024-06-15 19:40:47 +0200

    dojznd

--- admin/admin_bd.php
+++ admin/admin_bd.php
@@ -1,7 +1,7 @@
 <?php
 	session_start();
     include ("mysql.php");
-    $table = $_POST['capteur'];
+    $action = $_POST['capteur'];
     $ch1 = $_POST['ch1'];
     $ch2 = $_POST['ch2'];
     $ch3 = $_POST['ch3'];
@@ -9,18 +9,10 @@
     $ch5 = $_POST['ch5'];
     $ch6 = $_POST['ch6'];
 
-    // Récupérer la valeur du bouton radio sélectionné
-    if (isset($_POST['suppr'])) {
-        $action = $_POST['suppr'];
-        mysqli_query($id_bd, "DELETE FROM `$table` WHERE ");
-    } elseif (isset($_POST['mod'])) {
-        $action = $_POST['mod'];
-    } elseif (isset($_POST['add'])) {
-        $action = $_POST['add'];
-    }
+   
 
     // Afficher les valeurs récupérées
-    echo "Table selectionnee : " . htmlspecialchars($table) . "<br>";
+    echo "Table selectionnee : " . htmlspecialchars($action) . "<br>";
     echo "Champ 1 : " . htmlspecialchars($ch1) . "<br>";
     echo "Champ 2 : " . htmlspecialchars($ch2) . "<br>";
     echo "Champ 3 : " . htmlspecialchars($ch3) . "<br>";
