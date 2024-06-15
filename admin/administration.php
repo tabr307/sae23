347f082b514db17df5268786a7fc450a7a5e9dd8
valentin <valentin.lalue1998@gmail.com>
2024-06-15 19:35:24 +0200

    djzndib

--- admin/administration.php
+++ admin/administration.php
@@ -21,61 +21,79 @@
                 <img class="image" src="../ressources/bd.png" alt="bd">
             <form action="admin_bd.php" method="post">
                 <fieldset>
-                    <legend>Valeurs</legend> <!-- Titre du fieldset -->
+                    <legend>Administration</legend> <!-- Titre du fieldset -->
                     <p> 
-                        <label for="capteur">Sélectionnez la table dans laquelle modifier vos valeurs :</label><br>
+                        <label for="capteur">Sélectionnez les valeurs à ajouter/modifier/supprimer : </label><br>
                         <select id="capteur" name="capteur">
-                            <option value="administration">administration</option>
-                            <option value="batiment">batiment</option>
-                            <option value="capteur">capteur</option>
-                            <option value="mesures">mesures</option>
-                            <option value="salle">salle</option>
+                            <option value="administration">Ajouter</option>
+                            <option value="batiment">Modifier</option>
+                            <option value="capteur">Supprimer</option>
                         </select><br>
-                        <input type="text" id="ch1" name="ch1" size="70" placeholder="champ1"/><br>
-                        <input type="text" id="ch2" name="ch2" size="70" placeholder="champ2"/><br>
-                        <input type="text" id="ch3" name="ch3" size="70" placeholder="champ3"/><br>
-                        <input type="text" id="ch4" name="ch4" size="70" placeholder="champ4"/><br>
-                        <input type="text" id="ch5" name="ch5" size="70" placeholder="champ5"/><br>
-                        <input type="text" id="ch6" name="ch6" size="70" placeholder="champ6"/><br>
-                        <input type="radio" name="suppr" value="suppr" id="suppr" /><label for="suppr">supprimer</label>
-                        <input type="radio" name="mod" value="mod" id="mod" /><label for="mod">modifier</label>
-                        <input type="radio" name="add" value="add" id="add" /><label for="add">ajouter</label>
+                        <input type="text" id="ch1" name="ch1" size="70" placeholder="pseudo"/><br>
+                        <input type="text" id="ch2" name="ch2" size="70" placeholder="mdp"/><br>
                     </p>  
-            </fieldset>
+                </fieldset>
 
-            <fieldset>
-                    <legend>Tables</legend> <!-- Titre du fieldset -->
+                <fieldset>
+                    <legend>Mesures</legend> <!-- Titre du fieldset -->
                     <p> 
-                        <label for="capteur1">Sélectionnez la table :</label><br>
-                        <select id="capteur1" name="capteur1">
-                            <option value="administration">administration</option>
-                            <option value="batiment">batiment</option>
-                            <option value="capteur">capteur</option>
-                            <option value="mesures">mesures</option>
-                            <option value="salle">salle</option>
+                        <label for="capteur">Sélectionnez les valeurs à ajouter/modifier/supprimer : </label><br>
+                        <select id="capteur" name="capteur">
+                            <option value="administration">Ajouter</option>
+                            <option value="batiment">Modifier</option>
+                            <option value="capteur">Supprimer</option>
                         </select><br>
-                        <input type="text" id="champ0" name="champ0" size="70" placeholder="Nom de la table a créer"/><br>
-                        <input type="text" id="champ1" name="champ1" size="70" placeholder="comment apppeler le premier champ"/><br>
-                        <input type="text" id="champ2" name="champ2" size="70" placeholder="champ2"/><br>
-                        <input type="text" id="champ3" name="champ3" size="70" placeholder="champ3"/><br>
-                        <input type="text" id="champ4" name="champ4" size="70" placeholder="champ4"/><br>
-                        <input type="text" id="champ5" name="champ5" size="70" placeholder="champ5"/><br>
-                        <input type="text" id="champ6" name="champ6" size="70" placeholder="champ6"/><br>
-                        <input type="radio" name="suppr1" value="suppr1" id="suppr1" /><label for="suppr">supprimer la table</label>
-                        <input type="radio" name="mod1" value="mod1" id="mod1" /><label for="mod">modifier</label>
-                        <input type="radio" name="add1" value="add1" id="add1" /><label for="add">ajouter une table</label>
-                    </p>
-            </fieldset>
-            <fieldset>
-                    <legend>Base de données</legend> <!-- Titre du fieldset -->
+                        <input type="text" id="ch1" name="ch1" size="70" placeholder="id_mesure"/><br>
+                        <input type="text" id="ch2" name="ch2" size="70" placeholder="unite"/><br>
+                        <input type="text" id="ch3" name="ch3" size="70" placeholder="valeur"/><br>
+                        <input type="text" id="ch4" name="ch4" size="70" placeholder="date"/><br>
+                        <input type="text" id="ch5" name="ch5" size="70" placeholder="heure"/><br>
+                        <input type="text" id="ch6" name="ch6" size="70" placeholder="id_capteur"/><br>
+                    </p>  
+                </fieldset>
+                <fieldset>
+                    <legend>Capteur</legend> <!-- Titre du fieldset -->
                     <p> 
-                        <input type="radio" name="oui" value="oui" id="oui" /><label for="mod">Supprimer la BD pour de bon </label>
-                        <input type="radio" name="non" value="non" id="non" /><label for="add">Ne pas la supprimer</label><br><br>
-                        Êtes vous bien sur de votre choix ?  <br>
-                        <input type="checkbox" id="validation" name="validation" required />
-                        <label for="validation">Oui, je suis sur de mon choix</label><br>
-                    </p>
-            </fieldset>
+                        <label for="capteur">Sélectionnez les valeurs à ajouter/modifier/supprimer : </label><br>
+                        <select id="capteur" name="capteur">
+                            <option value="administration">Ajouter</option>
+                            <option value="batiment">Modifier</option>
+                            <option value="capteur">Supprimer</option>
+                        </select><br>
+                        <input type="text" id="ch1" name="ch1" size="70" placeholder="id_capteur"/><br>
+                        <input type="text" id="ch2" name="ch2" size="70" placeholder="nom_capteur"/><br>
+                        <input type="text" id="ch3" name="ch3" size="70" placeholder="id_salle"/><br>
+                    </p>  
+                </fieldset>
+                <fieldset>
+                    <legend>Salle</legend> <!-- Titre du fieldset -->
+                    <p> 
+                        <label for="capteur">Sélectionnez les valeurs à ajouter/modifier/supprimer : </label><br>
+                        <select id="capteur" name="capteur">
+                            <option value="administration">Ajouter</option>
+                            <option value="batiment">Modifier</option>
+                            <option value="capteur">Supprimer</option>
+                        </select><br>
+                        <input type="text" id="ch1" name="ch1" size="70" placeholder="id_salle"/><br>
+                        <input type="text" id="ch2" name="ch2" size="70" placeholder="nom_salle"/><br>
+                        <input type="text" id="ch3" name="ch3" size="70" placeholder="id_batiment"/><br>
+                    </p>  
+                </fieldset>
+                <fieldset>
+                    <legend>Batiment</legend> <!-- Titre du fieldset -->
+                    <p> 
+                        <label for="capteur">Sélectionnez les valeurs à ajouter/modifier/supprimer : </label><br>
+                        <select id="capteur" name="capteur">
+                            <option value="administration">Ajouter</option>
+                            <option value="batiment">Modifier</option>
+                            <option value="capteur">Supprimer</option>
+                        </select><br>
+                        <input type="text" id="ch1" name="ch1" size="70" placeholder="id_batiment"/><br>
+                        <input type="text" id="ch2" name="ch2" size="70" placeholder="nom_batiment"/><br>
+                        <input type="text" id="ch3" name="ch3" size="70" placeholder="pseudo"/><br>
+                        <input type="text" id="ch4" name="ch4" size="70" placeholder="mdp"/><br>
+                    </p>  
+                </fieldset>
             Avant de continuer, vous devez vérifier ces informations ! <br />
             <input type="checkbox" id="validation" name="validation" required />
             <label for="validation">Oui, j'ai pas relu et j'envoie sans le moindre doute !</label><br>
