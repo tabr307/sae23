437010e0a5828a28be94b0d4aea8712f5172aacd
Tabr <96288578+tabr307@users.noreply.github.com>
2025-03-23 11:12:22 +0100

    Update main.py

--- mqtt-client-script/main.py
+++ mqtt-client-script/main.py
@@ -7,17 +7,17 @@ from datetime import datetime
 
 # MQTT broker connexion configuration
 mqtt_config = {
-    'broker': 'mqtt.iut-blagnac.fr',
-    'port': 1883,
-    'rooms': ["E004", "E210", "B112", "B109"]
+    'broker': brokeraddress,
+    'port': x,
+    'rooms': ["n1", "n2", "n3", "n4"]
 }
 
 # MySQL database configuration
 db_config = {
-    'user': 'python',
-    'password': 'whzjWL5Z65yb8JYp',
-    'host': '127.0.0.1',
-    'database': 'sae23'
+    'user': username,
+    'password': password,
+    'host': hostip,
+    'database': dbname
 }
 
 ############################################### SCRIPT PART ########################################################### 
