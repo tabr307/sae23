f636f6b72d925a71d61b4bc1725f5a871db45eea
Tabr <96288578+tabr307@users.noreply.github.com>
2025-03-23 11:26:29 +0100

    Update gestionRT_nodered.php

--- web/gestionRT_nodered.php
+++ web/gestionRT_nodered.php
@@ -20,18 +20,18 @@
         <ul>
             <li>
                 <h3>Temperature<h3>
-                <iframe src="http://83.113.15.31:3000/d-solo/ddny9iezod8g0d/batiment-rt?orgId=1&from=1717756907329&to=1717778507329&panelId=1" width="500" height="500" frameborder="0"></iframe>
-                <iframe src="http://83.113.15.31:3000/d-solo/ddny9iezod8g0d/batiment-rt?orgId=1&from=1717756934289&to=1717778534290&panelId=4" width="500" height="500" frameborder="0"></iframe>
+                <iframe width="500" height="500" frameborder="0"></iframe>
+                <iframe width="500" height="500" frameborder="0"></iframe>
             </li>
             <li>
                 <h3>CO2</h3>
-                <iframe src="http://83.113.15.31:3000/d-solo/ddny9iezod8g0d/batiment-rt?orgId=1&from=1717756995038&to=1717778595038&panelId=2" width="500" height="500" frameborder="0"></iframe>
-                <iframe src="http://83.113.15.31:3000/d-solo/ddny9iezod8g0d/batiment-rt?orgId=1&from=1717757013451&to=1717778613451&panelId=5" width="500" height="500" frameborder="0"></iframe>
+                <iframe width="500" height="500" frameborder="0"></iframe>
+                <iframe width="500" height="500" frameborder="0"></iframe>
             </li>
             <li>
                 <h3>Humidité</h3>
-                <iframe src="http://83.113.15.31:3000/d-solo/ddny9iezod8g0d/batiment-rt?orgId=1&from=1717757066675&to=1717778666675&panelId=3" width="500" height="500" frameborder="0"></iframe>
-                <iframe src="http://83.113.15.31:3000/d-solo/ddny9iezod8g0d/batiment-rt?orgId=1&from=1717757081591&to=1717778681591&panelId=6" width="500" height="500" frameborder="0"></iframe>
+                <iframe width="500" height="500" frameborder="0"></iframe>
+                <iframe width="500" height="500" frameborder="0"></iframe>
             </li>
         </ul>
 
