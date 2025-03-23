69513d82405a7827ba3088089386ecb805778186
Tabr <96288578+tabr307@users.noreply.github.com>
2025-03-23 11:30:19 +0100

    Update mysql.php

--- web/mysql.php
+++ web/mysql.php
@@ -1,7 +1,7 @@
 <?php
 // connexion script
 
-  $id_bd = mysqli_connect("localhost", "rt", "passchroot" ,"sae23") //connecting the the sae23 DB with the rt login and passchroot password
+  $id_bd = mysqli_connect(*host*, *username*, *password* ,*dbname*) //connecting the the sae23 DB with the rt login and passchroot password
   //the address of the DB is localhost because this script is runnign on the server
     or die("Probleme de connexion a la bd");//if connexion impossible stop the processus and echo the text for the user
 
