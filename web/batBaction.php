9cbe6a74fbcf9fbec474c81e40386591dcbdf4eb
Tabr <96288578+tabr307@users.noreply.github.com>
2025-03-23 11:20:49 +0100

    Update batBaction.php

--- web/batBaction.php
+++ web/batBaction.php
@@ -140,13 +140,6 @@ echo "</table>";
 </section>
 </section>
 
-<footer>
-    <ul>
-        <li>Département Réseaux et Télécommunications</li>
-        <li>Cazettes, Le Deunff, Muller, Lalue</li>
-        <li>BUT1&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</li>
-    </ul>  
-</footer>
 </body>
 </html>
 
